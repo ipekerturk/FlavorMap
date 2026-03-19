dc347eafa27b896c281bc0c55e9463963509f839
İpek Ertürk <zipekerturk@gmail.com>
2026-03-19 15:29:18 +0300

    MS1 templates added

--- FlavorMap/urls.py
+++ FlavorMap/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 URL configuration for FlavorMap project.
 
--- main/views.py
+++ main/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 
-
 def home(request):
     return render(request, 'home.html')
 
 
-from django.shortcuts import render
+def restaurant_list(request):
+    restaurants = [
+        {'id': 1, 'name': 'Pizza Place', 'rating': 4.5},
+        {'id': 2, 'name': 'Sushi Bar', 'rating': 4.8},
+    ]
+    return render(request, 'restaurant_list.html', {'restaurants': restaurants})
 
-# Create your views here.
+
+def restaurant_detail(request, id):
+    restaurant = {
+        'id': id,
+        'name': 'Pizza Place',
+        'description': 'Best pizza in town',
+        'rating': 4.5
+    }
+    return render(request, 'restaurant_detail.html', {'restaurant': restaurant})
+
+
 def about(request):
     return render(request, 'about.html')
+
 
 def contact(request):
     return render(request, 'contact.html')
