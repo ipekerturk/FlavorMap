70a7ada332aa0657018f515bc398b8b80ec51e10
Zeynep Uygun <Zeynep.Uygun@live.acibadem.edu.tr>
2026-04-05 17:49:42 +0300

    Implemented user registration and authentication routing for MS3

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('restaurants/<int:id>/', views.restaurant_detail, name='restaurant_detail'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('register/', views.register, name='register'),
 ]
