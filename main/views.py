cec90299c23b1d5176892edc2f493edfdf528396
Zeynep Uygun <Zeynep.Uygun@live.acibadem.edu.tr>
2026-04-09 10:46:29 +0300

    imported login

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Restaurant, Review, Category, Location
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
