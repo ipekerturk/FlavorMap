e8e4312f6e2088857147ca35b52a379717548f4c
Zeynep Uygun <Zeynep.Uygun@live.acibadem.edu.tr>
2026-04-09 14:54:19 +0300

    defined register

--- main/views.py
+++ main/views.py
@@ -39,3 +39,15 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
