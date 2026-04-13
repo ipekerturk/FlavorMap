ea72690082da0f50b5c82d49930639009fb160bb
Zeynep Uygun <Zeynep.Uygun@live.acibadem.edu.tr>
2026-04-13 14:29:53 +0300

    added redirect

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Restaurant, Review, Category, Location
 from django.contrib.auth.forms import UserCreationForm
 
